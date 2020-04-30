81f209c221e1792547b58a24868e7e8fb34aebdc
David Dansby <ddansby89@gmail.com>
2020-04-30 01:20:14 -0700

    update hr_program fix code

--- employees.py
+++ employees.py
@@ -65,4 +65,4 @@ class SalesPerson(CommissionEmployee):
 # factory workers
 class FactoryWorker(HourlyEmployee):
     def work(self, hours):
-        print(f'{self.name} manufactures gadgets for {hours} hours.'
+        print(f'{self.name} manufactures gadgets for {hours} hours.')
